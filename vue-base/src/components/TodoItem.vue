db74007216d84b3d2793b060af7f160e83c13d77
Troy-S <troysheed@gmail.com>
2019-07-27 20:38:14 +0100

    AddTodo Component

--- vue-base/src/components/TodoItem.vue
+++ vue-base/src/components/TodoItem.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    <p>Todo Item</p>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
   </div>
 </template>
 
 <script>
   export default {
-    name: "TodoItem"
-  }
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+      markComplete() {
+        this.todo.completed = !this.todo.completed;
+      }
+    }
+  };
 </script>
+
+<style>
+ .todo-item {
+   background: #fff;
+   padding: 10px;
+   border-bottom: 1px #ccc dotted;
+ }
+
+ .is-complete {
+   text-decoration: line-through;
+ }
+ 
+ .del {
+   background: red;
+   border-radius: 50%;
+   border: 1px;
+   color: white;
+   padding: 5px 9px;
+ }
+</style>
