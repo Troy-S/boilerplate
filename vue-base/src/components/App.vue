768f71efb7bd4f65bb2d7fb7389920e06cdec7ec
Troy-S <troysheed@gmail.com>
2019-07-27 19:08:56 +0100

    Todos component

--- vue-base/src/components/App.vue
+++ vue-base/src/components/App.vue
@@ -1,16 +1,45 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <Todos/>
   </div>
 </template>
 
 <script>
+import Todos from '../components/Todos'
+
 export default {
   name: 'app',
+  components: {
+    Todos
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: false
+        }
+      ]
     }
   }
 }
 </script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+</style>
